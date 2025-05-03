6fe0014ddf55fc7910bc723fc6b0faba74011422
Deepak Muvva <152853891+deepakmuvva99@users.noreply.github.com>
2025-05-03 11:59:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from tensorflow.keras.models import load_model
@@ -27,4 +28,6 @@ def predict():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use dynamic port for Render or default to 5000 locally
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
